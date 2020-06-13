55c2501b4e204ad33d8a3f7ddb5b511e203981a5
Alex Lundquist <alex@alexlundquist.com>
2020-06-13 17:53:00 -0600

    adding Week 2's assignment finally

---
+++ week_01/xColorPicker/xColorPicker/UIColorExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIColorExtensions.swift
+//  xColorPicker
+//
+//  Created by Alex Lundquist on 6/1/20.
+//  Copyright © 2020 Alex Lundquist. All rights reserved.
+//
+
+import Foundation
---
+++ week_02/RGBullsEye/RGBullsEye/Models/Game.swift
@@ -0,0 +1,9 @@
+//
+//  Game.swift
+//  RGBullsEye
+//
+//  Created by Alex Lundquist on 6/13/20.
+//  Copyright © 2020 Ray Wenderlich. All rights reserved.
+//
+
+import Foundation

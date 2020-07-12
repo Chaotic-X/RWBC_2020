d254daa9b4878edd856fec1bc023a9c061704cc8
Alex Lundquist <alex@alexlundquist.com>
2020-07-11 22:04:50 -0600

    Getting my work up on GitHub

---
+++ week_07/Birdui/Birdui/PostView.swift
@@ -0,0 +1,30 @@
+//
+//  PostView.swift
+//  Birdui
+//
+//  Created by Audrey Tam on 4/7/20.
+//  Copyright © 2020 Razeware. All rights reserved.
+//
+
+import SwiftUI
+
+struct PostView: View {
+
+  var body: some View {
+    // TODO: This should look exactly like Birdie's table view cell.
+    // The post text is left-aligned below the mascot image.
+    // The image, if any, is horizontally centered in the view.
+    Text("Get it going")
+  }
+}
+
+struct PostView_Previews: PreviewProvider {
+  static var previews: some View {
+    PostView()
+  }
+}
+/*
+ post: MediaPost(textBody: "Went to the Aquarium today :]",
+ userName: "Audrey", timestamp: Date(timeIntervalSinceNow: -9876),
+ uiImage: UIImage(named: "octopus"))
+ */
